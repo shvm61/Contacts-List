5ff4df818afbb75de816d33592e76a07af838cae
shvm61 <60419370+shvm61@users.noreply.github.com>
2020-07-26 15:33:11 +0530

    last

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const path = require("path");
+const port = 8000;
+
+const db = require("./config/mongoose");
+const Contact = require("./models/contact");
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded());
+app.use(express.static("assets"));
+
+app.get("/", function (req, res) {
+  Contact.find({}, function (err, data) {
+    if (err) {
+      console.log("error in fetching contacts from db");
+      return;
+    }
+    return res.render("index", { contact_list: data });
+  });
+  //   res.render("index");
+});
+
+app.post("/create", function (req, res) {
+  Contact.create({ name: req.body.name, phone: req.body.phone }, function (
+    err,
+    data
+  ) {
+    if (err) {
+      console.log("Error in creating a contact!");
+      return;
+    }
+    return res.redirect("/");
+  });
+});
+app.get("/delete", function (req, res) {
+  Contact.findByIdAndDelete(req.query.id, function (err) {
+    if (err) {
+      console.log("error in deleting", err);
+      return;
+    }
+    return res.redirect("/");
+  });
+});
+app.listen(port, function (err) {
+  if (err) {
+    console.log("Error in running the server", err);
+  }
+  console.log("Yup!My Server is running on Port", port);
+});
